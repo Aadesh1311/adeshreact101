14f5c486172d281077014701ff60666453ae6f12
Aadesh1311 <100181537+Aadesh1311@users.noreply.github.com>
2022-05-22 13:47:07 +0530

    d
    d

---
+++ counter/myapp/src/component/Counter.jsx
@@ -0,0 +1,16 @@
+import {useState} from  "react"
+
+const Counter =()=>{
+    const [count,setCount]=useState(0)
+    return(
+        <div>
+            
+        <div> Counter :{count}</div>
+        <button onClick={()=>{setCount(count+1)}}>Increment</button>
+        <button onClick={()=>{setCount(count-1)}}>Decrement</button>
+        <button onClick={()=>{setCount(count*2)}}>Double</button>
+        </div>
+    )
+}
+
+export default Counter;
