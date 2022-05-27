49a81679a75b551fc337a6a5c2362a5a00b3dd94
Aadesh1311 <100181537+Aadesh1311@users.noreply.github.com>
2022-05-27 13:16:02 +0530

    ff
    ff

---
+++ form/myapp/src/form1.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const form1 = () => {
+    const[form ,setForm]=useState({})
+
+
+  return (
+    <div>
+        <form>
+        
+        
+        </form>
+    </div>
+  )
+}
+
+export default form1
